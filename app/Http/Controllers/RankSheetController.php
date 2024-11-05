d723035937d66f2f878784d97374b8ad5535c4b2
Samuel <samuel_cassar@hotmail.com>
2024-11-05 09:37:22 -0400

    fix rank sheet

--- app/Http/Controllers/RankSheetController.php
+++ app/Http/Controllers/RankSheetController.php
@@ -21,12 +21,11 @@ class RankSheetController extends Controller
 
     public function show(Request $request)
     {  
-        date_default_timezone_set('America/Caracas');
-
+        //return $this->summaryData($year, $term, $class_id);   
         $year = $request->year;
         $term = $request->term;
-        $className = $request->className;
-        $classGroup = $request->classGroup;
+        $class_id = $request->classId;   
+        date_default_timezone_set('America/Caracas');
         
         $termRecord = Term::where('id', $term)
         ->first();
@@ -34,18 +33,20 @@ class RankSheetController extends Controller
 
         $logo = public_path('/imgs/logo.png');        
         $school = config('app.school_name');        
-        $address = config('app.school_address_line_1');           
+        $address = config('app.school_address_line_1');   
+        $contact = config('app.school_contact_line_1');         
         $border = 1; 
 
         $this->pdf->SetAutoPageBreak(false);
         $this->pdf->AliasNbPages();
         $this->pdf->SetMargins(10, 10);              
         $this->pdf->AddPage('P', 'Letter');
-        $this->pdf->Image($logo, 10, 5, 25);
+        $this->pdf->Image($logo, 10, 8, 25);
         $this->pdf->SetFont('Times', 'B', '18');
         $this->pdf->MultiCell(0, 8, $school, 0, 'C' );
-        $this->pdf->SetFont('Times', 'I', 12);
+        $this->pdf->SetFont('Times', 'I', 10);
         $this->pdf->MultiCell(0, 4, $address, 0, 'C' );
+        $this->pdf->MultiCell(0, 4, $contact, 0, 'C' );
         $this->pdf->Ln(8);
 
         $this->pdf->SetFont('Times', 'B', 12);
@@ -60,7 +61,7 @@ class RankSheetController extends Controller
 
         $border = 0; 
         $this->pdf->SetFont('Times', '', 13);
-        $this->pdf->Cell(49, 9, $className, $border, 0, 'L');
+        $this->pdf->Cell(49, 9, $class_id, $border, 0, 'L');
         $this->pdf->Cell(49, 9, $year.'-'.($year+1), $border, 0, 'L');
         $this->pdf->Cell(49, 9, $report_term, $border, 0, 'L');
         $this->pdf->Cell(49, 9, 'End of Term', $border, 0, 'C');
@@ -70,28 +71,34 @@ class RankSheetController extends Controller
         $this->pdf->SetFont('Times', 'B', 11); 
         $this->pdf->Cell(25, 8, 'Student ID', $border, 0, 'L');
         $this->pdf->Cell(51, 8, 'Name', $border, 0, 'L');
-        $this->pdf->Cell(30, 8, 'Total (%)', $border, 0, 'C');
+        $this->pdf->Cell(30, 8, 'Avg (%)', $border, 0, 'C');
         $this->pdf->Cell(30, 8, 'Rank', $border, 0, 'C');
         $this->pdf->Cell(30, 8, 'Sessions Absent', $border, 0, 'C');
         $this->pdf->Cell(30, 8, 'Sessions Late', $border, 0, 'C');
         $this->pdf->Ln();
         $this->pdf->Ln(2);
 
-        $summary_data =  $this->summaryData($year, $term, $className, $classGroup);
-        // return $summary_data;
+        $summary_data =  $this->summaryData($year, $term, $class_id);
+        // return $summary_data; 
 
         foreach($summary_data as $index => $record){
             if($index%2 == 0)$this->pdf->SetFillColor(255,255,255);
             else $this->pdf->SetFillColor(220,220,220);
 
-            $rank = is_null($record['rank']) || $record['rank'] == 0 ? '-' : $record['rank'] ;
-
-            $student_average = ($record['average'] == 0) ? '-' : round($record['average'],1);
+            if($record['average'] == 0){
+                $student_average = '-';
+                $rank = '-';
+            }
+            else{
+                $student_average = ($record['average'] == 0);
+                $rank = $record['rank'];
+            }
+            $student_average = ($record['average'] == 0) ? '-' : number_format($record['average'],1);
             
             $this->pdf->SetFont('Times', '', 10);
             $this->pdf->Cell(25, 5, $record['student_id'], 0, 0, 'L', true);
             $this->pdf->Cell(51, 5, $record['name'], 0, 0, 'L', true);
-            $this->pdf->Cell(30, 5, number_format($record['average'], 1, '.', ''), 0, 0, 'C', true);
+            $this->pdf->Cell(30, 5, $student_average, 0, 0, 'C', true);
             $this->pdf->Cell(30, 5, $rank, 0, 0, 'C', true);
             $this->pdf->Cell(30, 5, $record['sessions_absent'], 0, 0, 'C', true);
             $this->pdf->Cell(30, 5, $record['sessions_late'], 0, 0, 'C', true);
@@ -117,10 +124,8 @@ class RankSheetController extends Controller
         exit;
     }
 
-    private function summaryData($year, $term, $className, $classGroup){
+    private function summaryData($year, $term, $class_id){
         $data = [];
-        $averages = [];
-
         $students_registered = Table1::join(
             'students', 
             'students.id', 
@@ -136,78 +141,106 @@ class RankSheetController extends Controller
         ->where([
             ['year', $year],
             ['term', $term],
-            ['table1.class_name', $className],
-            ['table1.class_group', $classGroup],
+            ['table1.class_id', $class_id]
         ])
         ->orderBy('students.last_name')
         ->orderBy('students.first_name')
         ->get();
 
+        $formClassRecord = FormClass::where('id', $class_id)
+        ->first();
+
+        $formLevel = $formClassRecord ? $formClassRecord->form_level : null;
+        $averages = array();
         foreach($students_registered as $student){
             $student_record = []; 
             $student_id = $student->student_id;
-            $student_average = $this->studentAverage($year, $term, $student_id);
-            $averages[] = $student_average;
-
             $student_record['student_id'] = $student_id;
             $student_record['name'] = $student->last_name.', '.$student->first_name;
-            $student_record['average'] =  $student_average;
-            // $student_record['rank'] = $this->rank($students_registered, $year, $term, $student_average);
+            $student_record['average'] =  $this->studentAverage($year, $term, $student_id, $formLevel, $averages);
+            // $student_record['rank'] = $this->rank($students_registered, $year, $term, $student_average, $formLevel);
             $student_record['sessions_absent'] = $student->times_absent;
             $student_record['sessions_late'] = $student->times_late;
             array_push($data, $student_record);
         }
 
-        foreach($data as &$record)
-        {
+        foreach($data as &$record){
             $record['rank'] = $this->rank($averages, $record['average']);
         }
+
+        unset($record);
 
         $data = $this->sort($data);
 
         return $data;
     }
 
-    private function studentAverage($year, $term, $student_id){
-        $participationWeightRecord = Weighting::where([
-            ['category', 'Participation'], 
-            ['year', '>=', $year],
-            ['term', '>=', $term],
-        ])
-        ->first();
-
-        $participationWeight = $participationWeightRecord ? $participationWeightRecord->weight : 0;
-
-        $table1Record = Table1::where([
+    private function studentAverage($year, $term, $student_id, $formLevel, &$averages)
+    {
+        $subjects = 0; 
+        $totalMarks = 0; 
+        $average = 0;
+        
+        $table2_records = Table2::select('exam_mark', 'course_mark')
+        ->where([
             ['year', $year],
             ['term', $term],
             ['student_id', $student_id]
-        ])
-        ->first();
+        ])->get();
 
-        if($table1Record)
-        {
-            $sharesCooperates = $table1Record->shares_cooperates;
-            $listensActively = $table1Record->listens_actively;
-            $persistent = $table1Record->persistent;
-            $acceptsChallenges = $table1Record->accepts_challenges;
-            $prepared = $table1Record->prepared;
+        foreach($table2_records as $record){
+            $subjects++;
+            $courseMark = $record->course_mark;
+            $examMark = $record->exam_mark;
+
+            if($term == 1 && (
+                $formLevel == 5 ||
+                $formLevel == 6 ||
+                $formLevel == 7
+            )
+            ){
+                $totalMarks += is_numeric($courseMark) ? $courseMark : 0; 
+            }
+            elseif($term == 2 && (
+                $formLevel == 1 ||
+                $formLevel == 2 ||
+                $formLevel == 3 ||
+                $formLevel == 4
+            )){
+                $totalMarks += is_numeric($courseMark) ? $courseMark : 0; 
+            }
+            elseif($term == 2 && (
+                    $formLevel == 5 ||
+                    $formLevel == 6 ||
+                    $formLevel == 7
+                )
+            )
+            {
+                $totalMarks += is_numeric($examMark) ? $examMark : 0; 
+            }
+            else {
+                $totalMarks += is_numeric($courseMark) ? number_format($courseMark*0.3,1) : 0; 
+                $totalMarks += is_numeric($examMark) ? number_format($examMark*0.7,1) : 0;
+            }
             
-            $participationTotal = $sharesCooperates + $listensActively + $persistent + $acceptsChallenges + $prepared;
-            $participationWeighted = floatval(round($participationTotal / $this->participationMaxTotal * $participationWeight, 0));
-            
-            return $table1Record->monthly_test + $table1Record->term_test + $table1Record->project + $participationWeighted;
         }
+
+        if($subjects != 0){
+            $average = $totalMarks / $subjects; 
+            $averages[] = $average;          
+        }
+        
+        return $average = $average != 0 ? $average : 0;
     }
 
-    private function rank($averages, $rank_average)
+    private function rank($averages, $key)
     {
         rsort($averages);
-
         foreach($averages as $index => $average){
-            if($average != 0 && $average == $rank_average ) return $index + 1;
+            if($average == $key){
+                return $index + 1;
+            }
         }
-
         return null;
     }
 
